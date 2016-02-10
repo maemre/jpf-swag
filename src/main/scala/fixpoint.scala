67c1a5af32190909e8be2696703c61215295319b
Mehmet Emre <maemre2@gmail.com>
2016-02-10 14:52:29 -0800

    Switch to new strategy for approximation.
    New strategy is to collect all abstract states then merge them. It is not a sound overapproximation but it is easier to implement for upcoming deadline.

--- src/main/scala/fixpoint.scala
+++ src/main/scala/fixpoint.scala
@@ -38,7 +38,7 @@ class FixpointListener(config: Config, jpf: JPF) extends PropertyListenerAdapter
   type Domain = NonrelationalDomain[Prefix, TrivialNumber]
 
   val methodToAnalyze = "twoifs".toLowerCase
-  var states = Map[Int, Domain]()
+  val states = MMap[Int, Set[Domain]]()
   jpf.addPublisherExtension(classOf[ConsolePublisher], this)
 
   override def executeInstruction(vm: VM, thread: ThreadInfo, insn: Instruction): Unit = {
@@ -54,15 +54,34 @@ class FixpointListener(config: Config, jpf: JPF) extends PropertyListenerAdapter
     }
     val pathCondition = Option(cg).map(cg ⇒ Helpers.parsePC(cg.getCurrentPC)).getOrElse(constraints.Conjunction())
 
-    // get values from stackx
-    val stack: IndexedSeq[StackValue] = thread.getTopFrame.operandAttrIterator.toIndexedSeq map {
-      case e:string.StringExpression ⇒ StringValue(Helpers.parseStrExpr(e))
-      case e:numeric.IntegerExpression ⇒ NumValue(Helpers.parseNumExpr(e))
-    }
-
-    val state: Domain = ???
+    // get values from stack
+    // TODO: It turns out that this doesn't work without a stack
+    val sf = thread.getTopFrame // stack frame
+    val stack: IndexedSeq[StackValue] =
+      for (i ← 0 to sf.getTopPos) yield
+        sf.getSlotAttr(i) match {
+          case null ⇒
+            if (sf.isReferenceSlot(i)) {
+              // TODO: create an abstract domain for references for this case
+              NumValue(NumConst(sf.getSlot(i)))
+            } else {
+              NumValue(NumConst(sf.getSlot(i)))
+            }
+          case e:string.StringExpression ⇒ StringValue(Helpers.parseStrExpr(e))
+          case e:numeric.IntegerExpression ⇒ NumValue(Helpers.parseNumExpr(e))
+        }
+    val state: Domain = NonrelationalDomain[Prefix, TrivialNumber]()
     state.construct(pathCondition, stack)
 
+    // Alternative strategy:
+    // Collect all states, then merge paths later on
+    if (! states.contains(pos)) {
+      states(pos) = Set(state)
+    } else {
+      states(pos) = states(pos) + state
+    }
+
+    /**
     if (! states.contains(pos)) {
       states = states + (pos → state)
     } else if (state ⊑ states(pos)) {
@@ -71,6 +90,7 @@ class FixpointListener(config: Config, jpf: JPF) extends PropertyListenerAdapter
       states = states + (pos → (states(pos) ⊔ state))
       // TODO: update stack acc. to state
     }
+      */
   }
 
   override def publishFinished(publisher: Publisher): Unit = {
@@ -79,8 +99,10 @@ class FixpointListener(config: Config, jpf: JPF) extends PropertyListenerAdapter
     publisher.publishTopicStart("Path conditions")
 
     for (insn ← states.keys.toSeq.sorted) {
+      // join all states that are seen so far
+      val state = states(insn).reduce(_ ⊔ _)
       // project to first value on stack
-      val constraint = states(insn).projectTo(0)
+      val constraint = state.projectTo(0)
       pw.println(s"$insn:\t${constraint}")
     }
   }
